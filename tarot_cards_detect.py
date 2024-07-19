254fd0f11a7212b6dc3e64193fb978f15cb11d90
taturazova <urazovasmtl@gmail.com>
2024-07-19 14:22:16 -0700

    Updating model & image object detection algorithm

--- tarot_cards_detect.py
+++ tarot_cards_detect.py
@@ -11,17 +11,11 @@ with open("card_data.json", "r") as file:
 
 def oneCardSpread(image):
     img_gray = cv2.imdecode(np.fromstring(image, np.uint8), cv2.IMREAD_GRAYSCALE)
-    approx = findOneCardPolygon(img_gray)
-    if approx is not None:
-        roi = adjustCardPerspective(img_gray, approx)
-        class_index = classifyCard(roi)
-        if class_index < len(card_list):
-            result = card_list[(class_index % 78)]
-            result["img_url"] = url_for(
-                "static", filename=f'card_images/{result["img"]}'
-            )
-            return card_list[(class_index % 78)]
-    return None
+    img_gray = resizeInputImage(img_gray, 600)
+    detectedCard = getOneCardFromImage(img_gray)
+    if detectedCard is not None:
+        return [str(classifyCard(detectedCard[0]))]
+    return []
 
 
 def classifyCard(roi):
@@ -41,24 +35,33 @@ def classifyCard(roi):
     return class_index
 
 
-def getCardJSONInfo(index):
-    return card_list[(index % 78)]
+def getOneCardFromImage(img_gray):
+    # Apply Gaussian Blur to reduce noise and improve edge detection
+    blurred = cv2.GaussianBlur(img_gray, (7, 7), 0)
 
+    # Define a sharpening kernel
+    sharpening_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
 
-def findOneCardPolygon(image):
-    # apply binary thresholding
-    _, thresh = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
+    # Apply the sharpening kernel to the grayscale image
+    sharpened = cv2.filter2D(blurred, -1, sharpening_kernel)
 
-    # detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+    # Use Canny edge detection
+    edges = cv2.Canny(sharpened, 150, 255)
+
+    # Find contours on the edges detected image using cv2.CHAIN_APPROX_SIMPLE
     contours, hierarchy = cv2.findContours(
-        image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE
+        edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
+
+    detectedCard = None
 
     for i, h in enumerate(hierarchy[0]):
         # Use polygon approximation to simplify the contour
+
         epsilon = 0.02 * cv2.arcLength(contours[i], True)
         approx = cv2.approxPolyDP(contours[i], epsilon, True)
         area = cv2.contourArea(contours[i])
+
         # Calculate the length of the contour
         contour_length = cv2.arcLength(contours[i], True)
         # Calculate the convex hull of the contour
@@ -69,48 +72,66 @@ def findOneCardPolygon(image):
 
         if ratio < 2 and len(approx) == 4 and area > 5000:
             rect = cv2.minAreaRect(approx)
-            _, (w, h), _ = rect
+            center, (w, h), angle = rect
             aspect_ratio = max(w, h) / min(w, h)
 
-            if 1.2 < aspect_ratio < 1.8:
-                return approx
-    return None
+            polygon_vertices = [point[0] for point in approx]
+            # Convert the polygon_vertices to NumPy array
+            polygon_vertices = np.array(polygon_vertices, dtype=np.float32)
+
+            polygon_area = cv2.contourArea(polygon_vertices)
+
+            if 1.2 < aspect_ratio < 1.8 and (
+                (detectedCard == None or polygon_area > detectedCard[1])
+            ):
+                # Determine the target width based on the maximum width of the base of the polygon
+                target_width = max(
+                    np.linalg.norm(polygon_vertices[1] - polygon_vertices[2]),
+                    np.linalg.norm(polygon_vertices[3] - polygon_vertices[0]),
+                )
+
+                # Calculate the target height based on the aspect ratio
+                target_height = int(target_width * aspect_ratio)
+
+                # Define the target rectangle coordinates
+                target_vertices = np.array(
+                    [
+                        [0, 0],
+                        [0, target_height],
+                        [target_width, target_height],
+                        [target_width, 0],
+                    ],
+                    dtype=np.float32,
+                )
+
+                # Compute the perspective transformation matrix
+                matrix = cv2.getPerspectiveTransform(polygon_vertices, target_vertices)
+
+                # Apply the perspective transformation
+                roi = cv2.warpPerspective(
+                    img_gray, matrix, (int(target_width), int(target_height))
+                )
+                detectedCard = (roi, polygon_area)
+    return detectedCard
 
 
-def adjustCardPerspective(image, approx, aspect_ratio=1.7):
-    polygon_vertices = [point[0] for point in approx]
-    # Determine the target width based on the maximum width of the base of the polygon
-    target_width = max(
-        np.linalg.norm(polygon_vertices[1] - polygon_vertices[2]),
-        np.linalg.norm(polygon_vertices[3] - polygon_vertices[0]),
-    )
+def resizeInputImage(image, new_height):
+    # Get the original dimensions
+    original_height, original_width = image.shape[:2]
 
-    # Calculate the target height based on the aspect ratio
-    target_height = int(target_width * aspect_ratio)
+    # Calculate the new width while maintaining the aspect ratio
+    aspect_ratio = original_width / original_height
+    new_width = int(new_height * aspect_ratio)
 
-    # Define the target rectangle coordinates
-    target_vertices = np.array(
-        [
-            [0, 0],
-            [0, target_height],
-            [target_width, target_height],
-            [target_width, 0],
-        ],
-        dtype=np.float32,
-    )
-
-    # Convert the polygon_vertices to NumPy array
-    polygon_vertices = np.array(polygon_vertices, dtype=np.float32)
-
-    # Compute the perspective transformation matrix
-    matrix = cv2.getPerspectiveTransform(polygon_vertices, target_vertices)
-
-    # Apply the perspective transformation
-    roi = cv2.warpPerspective(image, matrix, (int(target_width), int(target_height)))
-    return roi
+    # Resize the image
+    return cv2.resize(image, (new_width, new_height))
 
 
-# # Read Image
+def getCardJSONInfo(index):
+    return card_list[(index % 78)]
+
+
+# # # Read Image
 # image = cv2.imread("testImages/pageofcups.jpg")
 # cardJson = oneCardSpread(image)
 # # Print the predicted class
